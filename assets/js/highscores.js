3d69e11f8556ab4edbc96a49ac92bc75863330b4
SeanOVeryMac <seanovery1@gmail.com>
2021-09-19 16:09:17 -0400

    comments on js

--- assets/js/highscores.js
+++ assets/js/highscores.js
@@ -4,7 +4,7 @@ let   initialsArray = [],
       scoresSortable = [],
       sortedScoreNamesArray = [],
       sortedScoresArray = [];
-
+    // global variable declaration for highscores page
 function init() {
     let storedInitials = JSON.parse(localStorage.getItem('names'))
     if (storedInitials !== null) {
@@ -12,7 +12,7 @@ function init() {
     }
     parseSortScores();
     displayScores();
-}
+} // Function to pull from localstorage and run other functions to parse info pulled from local storage and display it.
 
 function parseSortScores() {
     for (i = 0; i < initialsArray.length; i++) {
@@ -23,7 +23,7 @@ function parseSortScores() {
     scoresSortable = Object.fromEntries(Object.entries(scoresObj).sort(([,a],[,b]) => b-a));
     sortedScoreNamesArray = Object.keys(scoresSortable);
     sortedScoresArray = Object.values(scoresSortable);
-}
+} // Function to take in info from local storage and put it in a usable form. Checks localstorage for only high scores by comparing to names in initialArray then stores all scores in an object. Object pairs sorted by value and stored in two sorted arrays where name and score will have matching indices.
 
 function displayScores() {
     for (i = 0; i < sortedScoreNamesArray.length; i++) {
@@ -33,6 +33,6 @@ function displayScores() {
         li.textContent = `${name}: ${score} points`;
         highscoreList.appendChild(li);
     }
-}
+} // Function using sorted name and score arrays to display sorted highscores on the page.
 
-init();
+init(); // Run init on opening webpage
--- assets/js/main.js
+++ assets/js/main.js
@@ -75,13 +75,14 @@ let   questions = [
       questionsAnswered = 0,
       initialsArray = [],
       timeCheck = score.wrong;
+      // global variable declaration for main.js
 
 function init() {
     let storedInitials = JSON.parse(localStorage.getItem('names'));
     if (storedInitials !== null) {
         initialsArray = storedInitials;
     }
-}
+} // Function run on page start to pull necessary info from local storage and store in initialsArray
 
 function startQuiz() {
     startBtn.classList.add('hide');
@@ -92,17 +93,17 @@ function startQuiz() {
     questionsIndex = 0;
     nextQuestion();
     timer();
-}
+} // Function to run necessary functions to start quiz. Hides buttons, randomizes the questions, displays questions, and starts timer.
 
 function timer() {
     let timeLeft = 60;
     let timeInterval = setInterval(function () {
-        timeLeft--;
-        timerEl.textContent = `${timeLeft} Seconds Left`;
         if (timeCheck !== score.wrong) {
             timeLeft -= 10;
             timeCheck = score.wrong;
         }
+        timeLeft--;
+        timerEl.textContent = `${timeLeft} Seconds Left`;
         if (timeLeft >= 0) {
             if (questionsAnswered === questions.length) {
                 clearInterval(timeInterval);
@@ -119,12 +120,12 @@ function timer() {
             questionContainerEl.classList.add('hide');
         }
     }, 1000)
-}
+} // Function to start and manage timer on quiz. Including things like showing the save score button.
 
 function nextQuestion() {
     reset();
     showQs(questions[questionsIndex]);
-}
+} // Function to go to next question. Removes previous question then displays next question.
 
 function randomArray(array) {
     for (let i = array.length-1; i > 0; i--) {
@@ -133,7 +134,7 @@ function randomArray(array) {
         array[i] = array[j];
         array[j] = t;
     }
-}
+} // Algorithm used to randomize an array.
 
 function showQs(q) {
     questionEl.textContent = q.question;
@@ -147,7 +148,7 @@ function showQs(q) {
         button.addEventListener('click', answerChoice);
         answerBtnsEl.appendChild(button)
     })
-}
+} // Function to create the answer buttons for the question and display those buttons and the question.
 
 function answerChoice(ev) {
     ev.preventDefault();
@@ -174,23 +175,22 @@ function answerChoice(ev) {
     } else {
         restartBtn.classList.remove('hide');
     }
-
-}
+} // Function to evaluate whether the button the user clicks is the correct choice or not and update necessary variables as well as the webpage dependent on that outcome. Also checks if all questions have been answered.
 
 function reset() {
     nextBtn.classList.add('hide');
     while (answerBtnsEl.firstChild) {
         answerBtnsEl.removeChild(answerBtnsEl.firstChild)
     }
-}
+} // Function which removes previous question's buttons.
 
-startBtn.addEventListener('click', startQuiz);
+startBtn.addEventListener('click', startQuiz); // Event listener to start quiz.
 
 nextBtn.addEventListener('click', function() {
     questionsIndex++
     questionContainerEl.classList.remove('hide')
     nextQuestion();
-})
+}) // Event listener to go to next question after quiz is started.
 
 restartBtn.addEventListener('click', function() {
     score.correct = 0;
@@ -198,7 +198,7 @@ restartBtn.addEventListener('click', function() {
     scoreEl.innerText = `Correct: ${score.correct}
     Incorrect: ${score.wrong}`;
     startQuiz();
-})
+}) // Event listener to restart the quiz upon completion.
 
 saveScoreBtn.addEventListener('click', function() {
     saveScoreContainer.classList.remove('hide');
@@ -210,6 +210,6 @@ saveScoreBtn.addEventListener('click', function() {
         localStorage.setItem(`${userName}`, score.points);
         location.href = 'highscores.html'
     })
-})
+}) // Event listeners to show score saving input and push necessary info into variables then into local storage.
 
-init();
+init(); // Run init on opening page.
